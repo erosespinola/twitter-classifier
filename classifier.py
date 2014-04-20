30879b9930a4c9d1d48c36bc0ae92f8cffb80410
erosespinola <eros.espinola.gonzalez@gmail.com>
2014-04-20 13:12:58 -0600

    naive bayes classifier implemented

--- classifier.py
+++ classifier.py
@@ -1,16 +1,61 @@
 import nltk
 import numpy as np
+import cPickle as pickle
+import sys
 
-def read_tweets(file):
-	tweets = np.load(file)
-	return tweets
+def read_file(file):
+	data = np.load(file)
+	return data
 
-def save_tweets(file):
+def get_tweets(file):
 	tweets = []
 	for line in open(file, "r"):
 		params = line.split(",")
-		tweet = params[-1]
-		s_tweet = tweet[1:len(tweet)-2].strip('\n')
-		label = [False,True][params[0] == '"4"']
-		tweets.append([label, s_tweet])
-	np.save("dataset/training", np.array(tweets))
+		tweet = params[-1].strip("\n")[1:len(params[-1])-2]
+		tweet = [w.lower() for w in tweet.split() if len(w) >= 3]
+		label = ["negative", "possitive"][params[0] == '"4"']
+		tweets.append((tweet, label))
+	np.save("dataset/training", np.array(tweets, dtype=object))
+
+def get_words(tweets):
+	words = []
+	for t in tweets:
+		words.extend(t[0])
+	np.save("dataset/words", np.array(words))
+
+def get_features(words):
+	words = nltk.FreqDist(words)
+	features = words.keys()
+	np.save("dataset/features", np.array(features))
+
+def analize_tweet(tweet):
+	tweet_words = set(tweet)
+	tweet_features = {}
+	for word in features:
+		tweet_features[word] = (word in tweet_words)
+	return tweet_features
+
+# MAIN
+
+# get_tweets("dataset/training_reduced.csv")
+tweets = read_file("dataset/training.npy")
+# print tweets
+
+# get_words(tweets.tolist())
+# words = read_file("dataset/words_reduced.npy")
+# print words
+
+# get_features(words.tolist())
+features = read_file("dataset/features.npy")
+# print features
+
+training_set = nltk.classify.apply_features(analize_tweet, tweets.tolist())
+classifier = nltk.NaiveBayesClassifier.train(training_set)
+
+print classifier.show_most_informative_features(10)
+
+while True:
+	tweet = raw_input("Enter a tweet: ")
+	if tweet == "exit()":
+		sys.exit()
+	print classifier.classify(analize_tweet(tweet.split()))
