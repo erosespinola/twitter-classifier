f686453a59967c7c4703015297e022e515909030
erosespinola <eros.espinola.gonzalez@gmail.com>
2014-04-26 18:17:13 -0600

    server implemented

--- classifier.py
+++ classifier.py
@@ -1,16 +1,37 @@
 import sys
 import nltk
+import time
 import tweepy
 import string
+import SocketServer
 import nltk.classify
+import BaseHTTPServer
 from nltk import bigrams
 from nltk.corpus import stopwords
 from sklearn.svm import LinearSVC
 from nltk.stem.lancaster import LancasterStemmer
 
-def read_file(file):
-	data = np.load(file)
-	return data
+class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
+    
+	def do_HEAD(s):
+		s.send_response(200)
+		s.send_header("Content-type", "text/html")
+		self.send_header("Access-Control-Allow-Origin", "*");
+		self.send_header("Access-Control-Expose-Headers", "Access-Control-Allow-Origin");
+		self.send_header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+		s.end_headers()
+
+	def do_GET(s):
+		s.send_response(200)
+		path = s.path.split("=")
+		user = path[-1]
+		result = analyze_account(user, 100)
+		s.send_header("Content-type", "text/html")
+		s.end_headers()
+		s.wfile.write("<html><body>")
+		s.wfile.write("<p>" + str(user) + "</p>")
+		s.wfile.write("<p>" + str(result) + "</p>")
+		s.wfile.write("</body></html>")
 
 def get_tweets(file):
 	tweets = []
@@ -21,7 +42,6 @@ def get_tweets(file):
 		label = params[0]
 		tweets.append((tweet, label))
 	return tweets
-	# np.save("dataset/training", np.array(tweets, dtype=object))
 
 def get_words(tweets):
 	words = []
@@ -30,13 +50,11 @@ def get_words(tweets):
 		# for t in bigrams(tweet):
 		# 	words.extend([" ".join(t)])
 	return words
-	# np.save("dataset/words", np.array(words))
 
 def get_features(words):
 	words = nltk.FreqDist(words)
 	features = words.keys()
 	return features
-	# np.save("dataset/features", np.array(features))
 
 def analize_tweet(tweet):
 	tweet_words = set(tweet)
@@ -55,6 +73,30 @@ def clean_tweet(tweet):
 			clean_tweet += st.stem(token) + " "
 	clean_tweet = [w.lower() for w in clean_tweet.split() if not w in stopwords.words('english') and len(w) >= 3]
 	return clean_tweet
+
+def get_accuracy(filename, samples):
+	c = 0
+	for line in open(filename, "r"):
+		params = line.split(",\"")
+		tweet = params[-1].strip("\n")[:len(params[-1])-2]
+		tweet = clean_tweet(tweet)
+		label = params[0]
+		if label == classifier.classify(analize_tweet(tweet)):
+			c += 1
+	return (c/samples) * 100
+
+def analyze_account(user, num):
+	tweets = twitter_fetch(user, num)
+	positive = 0
+	negative = 0
+	for tweet in tweets:
+		tweet = clean_tweet(tweet)
+		sentiment = classifier.classify(analize_tweet(tweet))
+		if sentiment == "\"0\"":
+			negative += 1
+		else:
+			positive += 1
+	return (positive, negative)
 
 def twitter_fetch(screen_name, num_tweets):
 	ckey = "peTdiI7qoZGme5vHlJyh0OKd7"
@@ -75,20 +117,13 @@ def twitter_fetch(screen_name, num_tweets):
 	return tweets
 
 # MAIN
-
 print "Reading tweets...\n"
-# get_tweets("dataset/training.txt")
-# tweets = read_file("dataset/training.npy")
 tweets = get_tweets("dataset/training.txt")
 
 print "Getting words...\n"
-# get_words(tweets.tolist())
-# words = read_file("dataset/words.npy")
 words = get_words(tweets)
 
 print "Getting features...\n"
-# get_features(words.tolist())
-# features = read_file("dataset/features.npy")
 features = get_features(words)
 
 print "Training...\n"
@@ -97,45 +132,23 @@ training_set = nltk.classify.apply_features(analize_tweet, tweets)
 print "Getting classifier...\n"
 # classifier = nltk.classify.SklearnClassifier(LinearSVC())
 # classifier.train(training_set)
-
 classifier = nltk.NaiveBayesClassifier.train(training_set)
-print classifier.show_most_informative_features(30)
+# print classifier.show_most_informative_features(30)
 
-# Twitter
-while True:
-	user = raw_input("Enter a user: ")
-	tweets = twitter_fetch(user, 10)
-	positive = 0
-	negative = 0
-	for tweet in tweets:
-		tweet = clean_tweet(tweet)
-		sentiment = classifier.classify(analize_tweet(tweet))
-		if sentiment == "\"0\"":
-			negative += 1
-		else:
-			positive += 1
-	print "Positive: " + str(positive) + "%"
-	print "Negative: " + str(negative) + "%"
+# print analyze_account("oprah", 100)
+# print analyze_account("billgates", 100)
+# print analyze_account("emwatson", 100)
+# print analyze_account("google", 100)
 
-# Validation
-# c = 0
-# for line in open("dataset/test.csv", "r"):
-# 	params = line.split(",\"")
-# 	tweet = params[-1].strip("\n")[:len(params[-1])-2]
-# 	tweet = clean_tweet(tweet)
-# 	label = params[0]
-# 	if label == classifier.classify(analize_tweet(tweet)):
-# 		c += 1
-# print "Accuracy: " + str((c/359.0) * 100)
+HOST_NAME = "127.0.0.1"
+PORT_NUMBER = 9000
 
-# User input
-# while True:
-# 	tweet = raw_input("Enter a tweet: ")
-# 	if tweet == "exit()":
-# 		sys.exit()
-# 	st = LancasterStemmer()
-# 	tweet_stem = ""
-# 	for token in tweet.split():
-# 		tweet_stem += st.stem(token) + " "
-# 	filtered_tweet = [w for w in tweet_stem.split() if not w in stopwords.words('english')]
-# 	print classifier.classify(analize_tweet(tweet_stem))
+server_class = BaseHTTPServer.HTTPServer
+httpd = server_class((HOST_NAME, PORT_NUMBER), MyHandler)
+print time.asctime(), "Server Starts - %s:%s" % (HOST_NAME, PORT_NUMBER)
+try:
+	httpd.serve_forever()
+except KeyboardInterrupt:
+	pass
+httpd.server_close()
+print time.asctime(), "Server Stops - %s:%s" % (HOST_NAME, PORT_NUMBER)
